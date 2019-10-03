1c7970238d0beee528d21755cbd0bddd10881a1e
Ingrid do Rio Schulze <ingriddorio@gmail.com>
2019-10-03 22:20:53 +0200

    initial commit, add all project files and documentation

---
+++ README.md
@@ -0,0 +1,40 @@
+# about
+
+Todo list app (like all the other ones) that allows user to add and delete todos.
+
+# technologies
+
+- React
+- Hooks
+- Theme: Material-UI
+
+
+# instructions
+
+1.  clone repository
+
+    ```bash
+    git clone https://github.com/ingriddorioschulze/todo-list
+    cd todo-list
+    ```
+
+2.  install dependencies
+
+    ```bash
+    npm install
+    ```
+
+3.  start the application on your local machine
+
+    ```bash
+    npm start
+    ```
+
+# credits
+
+- Free Code Camp Tutorial
+  https://www.freecodecamp.org/news/how-to-build-a-todo-list-with-react-hooks-ebaa4e3db3b/
+
+#
+
+with :yellow_heart: by [Ingrid do Rio Schulze](https://github.com/ingriddorioschulze)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Typography from '@material-ui/core/Typography';
-import './styles.css';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import Typography from '@material-ui/core/Typography'
+import TodoForm from './TodoForm'
+import TodoList from './TodoList'
+import useTodoState from './useTodoState'
+import './styles.css'
 
 const App = () => {
+  const { todos, addTodo, deleteTodo } = useTodoState([])
+
   return (
     <div className="App">
       <Typography component="h1" variant="h2">
-        Todos
+        todos
       </Typography>
-    </div>
-  );
-};
+      <TodoForm
+        saveTodo={todoText => {
+          const trimmedText = todoText.trim()
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+          if (trimmedText.length > 0) {
+            addTodo(trimmedText)
+          }
+        }}
+      />
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
+    </div>
+  )
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
